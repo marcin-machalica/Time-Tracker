f346004d87ad4566f02ef3919061bc8dc0099687
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-03-14 20:50:28 +0100

    Added validation for activity model, improved validation for controller's interaction with activities, improved code clarity

--- src/machalica/marcin/timetracker/main/MainController.java
+++ src/machalica/marcin/timetracker/main/MainController.java
@@ -11,8 +11,8 @@ import javafx.scene.layout.HBox;
 import javafx.util.StringConverter;
 import machalica.marcin.timetracker.model.Activity;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class MainController {
     @FXML
@@ -33,24 +33,27 @@ public class MainController {
     private TextField infoInput;
     @FXML
     private Button addActivityButton;
+    @FXML
+    private Label warningLabel;
 
     private ObservableList<Activity> activities = FXCollections.observableArrayList();
-    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @FXML
     private void initialize() {
         dateColumn.setCellValueFactory(new PropertyValueFactory<Activity, String>("date"));
         timeColumn.setCellValueFactory(new PropertyValueFactory<Activity, String>("time"));
         infoColumn.setCellValueFactory(new PropertyValueFactory<Activity, String>("info"));
+        setupActionButtonsColumnCellFactories();
 
         activityTable.setItems(activities);
 
-        dateInput.setPromptText("DD/MM/YYYY");
-        timeInput.setPromptText("00:00");
         setupDateInputConverter();
         setupDateInputListener();
         setupAddActivityButtonListeners();
-        setupActionButtonsColumnCellFactories();
+
+        dateInput.setPromptText("DD/MM/YYYY");
+        timeInput.setPromptText("00:00");
+        dateInput.requestFocus();
     }
 
     private void setupDateInputListener() {
@@ -90,7 +93,7 @@ public class MainController {
         dateInput.setConverter(new StringConverter<LocalDate>() {
             @Override
             public String toString(LocalDate localDate) {
-                return localDate == null ? null : dateTimeFormatter.format(localDate);
+                return localDate == null ? null : Activity.DATE_TIME_FORMATTER.format(localDate);
             }
 
             @Override
@@ -99,7 +102,7 @@ public class MainController {
                 {
                     return null;
                 }
-                return LocalDate.parse(dateString, dateTimeFormatter);
+                return LocalDate.parse(dateString, Activity.DATE_TIME_FORMATTER);
             }
         });
     }
@@ -112,7 +115,7 @@ public class MainController {
         if (localDate == null) {
             dateInput.requestFocus();
             return;
-        } else if (time.equals("") || !time.matches("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")) {
+        } else if (time.equals("") || !time.matches(Activity.TIME_PATTERN)) {
             timeInput.requestFocus();
             return;
         } else if (info.equals("")){
@@ -120,18 +123,21 @@ public class MainController {
             return;
         }
 
-        String date = dateTimeFormatter.format(localDate);
-
-        activities.add(new Activity(
-                        date,
-                        time,
-                        info
-        ));
-
-        dateInput.setValue(null);
-        timeInput.clear();
-        infoInput.clear();
-        dateInput.requestFocus();
+        try {
+            activities.add(new Activity(
+                    localDate,
+                    time,
+                    info
+            ));
+            dateInput.setValue(null);
+            timeInput.clear();
+            infoInput.clear();
+            warningLabel.setText("");
+        } catch (DateTimeException | IllegalArgumentException ex) {
+            warningLabel.setText(ex.getMessage());
+        } finally {
+            dateInput.requestFocus();
+        }
     }
 
     private void setupAddActivityButtonListeners() {
--- src/machalica/marcin/timetracker/model/Activity.java
+++ src/machalica/marcin/timetracker/model/Activity.java
@@ -2,22 +2,38 @@ package machalica.marcin.timetracker.model;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Activity {
     private final SimpleStringProperty date;
     private final SimpleStringProperty time;
     private final SimpleStringProperty info;
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final String TIME_PATTERN = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$";
 
-    public Activity(String date, String time, String info) {
+    public Activity(LocalDate localDate, String time, String info) throws DateTimeException, IllegalArgumentException {
+        if(localDate == null) { throw new IllegalArgumentException("LocalDate object cannot be null"); }
+        if(time == null) { throw new IllegalArgumentException("Time cannot be null"); }
+        if(info == null) { throw new IllegalArgumentException("Info cannot be null"); }
+
+        String date = DATE_TIME_FORMATTER.format(localDate);
+        if (time.equals("") || !time.matches(TIME_PATTERN)) { throw new IllegalArgumentException("Wrong time format"); }
+        if (info.trim().equals("")) { throw new IllegalArgumentException("Info cannot be empty"); }
+
         this.date = new SimpleStringProperty(date);
         this.time = new SimpleStringProperty(time);
-        this.info = new SimpleStringProperty(info);
+        this.info = new SimpleStringProperty(info.trim());
     }
 
     public String getDate() {
         return date.get();
     }
 
-    public void setDate(String date) {
+    public void setDate(LocalDate localDate) throws DateTimeException, IllegalArgumentException {
+        if(localDate == null) { throw new IllegalArgumentException("LocalDate object cannot be null"); }
+        String date = DATE_TIME_FORMATTER.format(localDate);
         this.date.set(date);
     }
 
@@ -25,7 +41,9 @@ public class Activity {
         return time.get();
     }
 
-    public void setTime(String time) {
+    public void setTime(String time) throws IllegalArgumentException {
+        if(time == null) { throw new IllegalArgumentException("Time cannot be null"); }
+        if (time.equals("") || !time.matches(TIME_PATTERN)) { throw new IllegalArgumentException("Wrong time format"); }
         this.time.set(time);
     }
 
@@ -33,7 +51,9 @@ public class Activity {
         return info.get();
     }
 
-    public void setInfo(String info) {
+    public void setInfo(String info) throws IllegalArgumentException {
+        if(info == null) { throw new IllegalArgumentException("Info cannot be null"); }
+        if (info.trim().equals("")) { throw new IllegalArgumentException("Info cannot be empty"); }
         this.info.set(info);
     }
 }
