ed3ee32ed34d198c5feb8d0f0aa06d74df7862c3
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-03-18 00:25:59 +0100

    Added shorthand syntax for datePickers and time inputs, time inputs cleanse themselves before checking, info inputs trim themselves and capitalize before checking

--- src/main/java/machalica/marcin/timetracker/main/MainController.java
+++ src/main/java/machalica/marcin/timetracker/main/MainController.java
@@ -33,6 +33,8 @@ import java.sql.SQLException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainController {
     @FXML
@@ -83,10 +85,117 @@ public class MainController {
         setupDateInputConverter(dateInput);
         setupDateInputListener(dateInput);
         setupAddActivityButtonListeners();
+        setupShorthandSyntaxListeners(dateInput, timeInput, infoInput);
 
         dateInput.setPromptText("DD/MM/YYYY");
-        timeInput.setPromptText("00:00");
+        timeInput.setPromptText("0:00");
         Platform.runLater(() -> dateInput.requestFocus());
+    }
+
+    private void setupShorthandSyntaxListeners(DatePicker dateInput, TextField timeInput, TextField infoInput) {
+        dateInput.focusedProperty().addListener(((observable, oldValue, newValue) -> {
+            if(!newValue) {
+                Matcher matcher = Pattern.compile("^[yY]+$").matcher(dateInput.getEditor().getText());
+                boolean wasFound = matcher.find();
+                int count = 0;
+                char foundCharacter = '_';
+
+                if (wasFound) {
+                    count = matcher.group().split("").length;
+                    foundCharacter = 'y';
+                } else {
+                    matcher = Pattern.compile("^[tT]+$").matcher(dateInput.getEditor().getText());
+                    wasFound = matcher.find();
+
+                    if(wasFound) {
+                        count = matcher.group().split("").length;
+                        foundCharacter = 't';
+                    } else {
+                        if (dateInput.getEditor().getText().matches("^[dD]+$")) {
+                            count = 1;
+                            foundCharacter = 'd';
+                        }
+                    }
+                }
+                if (count > 0) {
+                    long todayInEpochDays = LocalDate.now().toEpochDay();
+                    long pastDateInEpochDays = todayInEpochDays - count;
+                    long futureDateInEpochDays = todayInEpochDays + count;
+                    switch (foundCharacter) {
+                        case 'y':
+                            LocalDate pastLocalDate = LocalDate.ofEpochDay(pastDateInEpochDays);
+                            dateInput.setValue(pastLocalDate);
+                            break;
+                        case 'd':
+                            LocalDate todayLocalDate = LocalDate.ofEpochDay(todayInEpochDays);
+                            dateInput.setValue(todayLocalDate);
+                            break;
+                        case 't':
+                            LocalDate futureLocalDate = LocalDate.ofEpochDay(futureDateInEpochDays);
+                            dateInput.setValue(futureLocalDate);
+                            break;
+                    }
+                }
+            }
+        }));
+
+        timeInput.focusedProperty().addListener(((observable, oldValue, newValue) -> {
+            if(!newValue) {
+                timeInput.setText(timeInput.getText().replaceAll(" +", " ").replaceAll("[^0-9: ]", "").trim());
+                String time = "";
+
+                Matcher matcher = Pattern.compile("^([0-1]?[0-9]|2[0-3])$").matcher(timeInput.getText());
+                boolean wasFound = matcher.find();
+
+                if (wasFound) {
+                    if(matcher.group(0).length() == 2 && matcher.group(0).startsWith("0")) {
+                        time = matcher.group().replaceFirst("0", "") + ":00";
+                    } else {
+                        time = matcher.group(0) + ":00";
+                    }
+                } else {
+                    matcher = Pattern.compile("^(2[4-9]|[3-5][0-9])$").matcher(timeInput.getText());
+                    wasFound = matcher.find();
+
+                    if (wasFound) {
+                        time = "0:" + matcher.group(0);
+                    } else {
+                        matcher = Pattern.compile("^([0-1]?[0-9]|2[0-3]) ([0-5]?[0-9])$").matcher(timeInput.getText());
+                        wasFound = matcher.find();
+
+                        if (wasFound) {
+                            String[] timeParts = matcher.group(0).split(" ");
+                            String hours = (timeParts[0].length() == 2 && timeParts[0].startsWith("0")) ? timeParts[0].replaceFirst("0", "") : timeParts[0];
+                            String minutes = timeParts[1].length() == 1 ? "0" + timeParts[1] : timeParts[1];
+                            time = hours + ":" + minutes;
+                        }
+                    }
+                }
+
+                if(!time.equals("")) {
+                    timeInput.setText(time);
+                }
+            }
+        }));
+
+        infoInput.focusedProperty().addListener(((observable, oldValue, newValue) -> {
+            if(!newValue) {
+                infoInput.setText(infoInput.getText().trim());
+                
+                if(infoInput.getText().length() > 0) {
+                    char firstChar = infoInput.getText().charAt(0);
+                    if(Character.isLetter(firstChar)) {
+                        firstChar = Character.toUpperCase(firstChar);
+
+                        if(infoInput.getText().length() > 1) {
+                            infoInput.setText(firstChar + infoInput.getText().substring(1));
+                        } else {
+                            infoInput.setText(String.valueOf(firstChar));
+                        }
+                    }
+                }
+            }
+        }));
     }
 
     private boolean onExit() {
@@ -397,12 +506,14 @@ public class MainController {
         setupDateInputListener(editDateInput);
 
         final TextField editTimeInput = new TextField();
-        editTimeInput.setPromptText("00:00");
+        editTimeInput.setPromptText("0:00");
         editTimeInput.setText(activity.getTime());
 
         final TextField editInfoInput = new TextField();
         editInfoInput.setPromptText("Info");
         editInfoInput.setText(activity.getInfo());
+
+        setupShorthandSyntaxListeners(editDateInput, editTimeInput, editInfoInput);
 
         final Label editWarningLabel = new Label();
         editWarningLabel.setTextFill(Color.valueOf("#cc3300"));
