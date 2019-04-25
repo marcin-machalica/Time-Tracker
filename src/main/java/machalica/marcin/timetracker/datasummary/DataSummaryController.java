aa7af2bd29078e2746d626507073e2ab46916595
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-04-25 03:17:01 +0200

    Summary is now prepared from the selected time period

--- src/main/java/machalica/marcin/timetracker/datasummary/DataSummaryController.java
+++ src/main/java/machalica/marcin/timetracker/datasummary/DataSummaryController.java
@@ -12,6 +12,10 @@ import machalica.marcin.timetracker.model.Activity;
 import machalica.marcin.timetracker.model.DatePickerHelper;
 import javafx.scene.chart.XYChart;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
 import static machalica.marcin.timetracker.datasummary.DateOption.*;
 
 public class DataSummaryController {
@@ -36,9 +40,25 @@ public class DataSummaryController {
         ShorthandSyntaxHelper.setupDateShorthandSyntaxListener(startDateInput);
         ShorthandSyntaxHelper.setupDateShorthandSyntaxListener(endDateInput);
 
+        startDateInput.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (dateOptionChoiceBox.getValue().equals(BETWEEN)) {
+                setBarChartData(BETWEEN);
+            }
+        }));
+
+        endDateInput.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (dateOptionChoiceBox.getValue().equals(BETWEEN)) {
+                setBarChartData(BETWEEN);
+            }
+        }));
+
         dateOptionChoiceBox.getItems().setAll(
                 LAST_WEEK, LAST_MONTH, LAST_YEAR, ALL, BETWEEN
         );
+
+        dateOptionChoiceBox.getSelectionModel().selectedItemProperty().addListener(((observable, oldValue, newValue) -> {
+            setBarChartData(newValue);
+        }));
     }
 
     private void setupChart() {
@@ -55,6 +75,88 @@ public class DataSummaryController {
             }
             dataChart.getData().addAll(series1);
         });
+    }
+
+    private void setBarChartData(DateOption dateOption) {
+        dataChart.getData().clear();
+        XYChart.Series series1 = new XYChart.Series();
+
+        switch (dateOption) {
+            case LAST_WEEK:
+                Platform.runLater(() -> {
+                    for (Activity activity : activities) {
+                        if (activity.getDate().before(Date.from(ZonedDateTime.now().toInstant())) &&
+                                activity.getDate().after(Date.from(ZonedDateTime.now().minusWeeks(1).toInstant()))) {
+                            series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                        }
+                    }
+                    dataChart.getData().addAll(series1);
+                });
+                break;
+            case LAST_MONTH:
+                Platform.runLater(() -> {
+                    for (Activity activity : activities) {
+                        if (activity.getDate().before(Date.from(ZonedDateTime.now().toInstant())) &&
+                                activity.getDate().after(Date.from(ZonedDateTime.now().minusMonths(1).toInstant()))) {
+                            series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                        }
+                    }
+                    dataChart.getData().addAll(series1);
+                });
+                break;
+            case LAST_YEAR:
+                Platform.runLater(() -> {
+                    for (Activity activity : activities) {
+                        if (activity.getDate().before(Date.from(ZonedDateTime.now().toInstant())) &&
+                                activity.getDate().after(Date.from(ZonedDateTime.now().minusYears(1).toInstant()))) {
+                            series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                        }
+                    }
+                    dataChart.getData().addAll(series1);
+                });
+                break;
+            case ALL:
+                Platform.runLater(() -> {
+                    for (Activity activity : activities) {
+                        series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                    }
+                    dataChart.getData().addAll(series1);
+                });
+                break;
+            case BETWEEN:
+                Platform.runLater(() -> {
+                    LocalDate startLocalDate = startDateInput.getValue();
+                    LocalDate endLocalDate = endDateInput.getValue();
+                    if (startLocalDate != null && endLocalDate != null) {
+                        Date startDate = Date.valueOf(LocalDate.ofEpochDay(startLocalDate.toEpochDay() - 1));
+                        Date endDate = Date.valueOf(LocalDate.ofEpochDay(endLocalDate.toEpochDay() + 1));
+
+                        for (Activity activity : activities) {
+                            if (activity.getDate().after(startDate) && activity.getDate().before(endDate)) {
+                                series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                            }
+                        }
+                    } else if (startLocalDate != null) {
+                        Date startDate = Date.valueOf(LocalDate.ofEpochDay(startLocalDate.toEpochDay() - 1));
+                        for (Activity activity : activities) {
+                            if (activity.getDate().after(startDate)) {
+                                series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                            }
+                        }
+                    } else if (endLocalDate != null) {
+                        Date endDate = Date.valueOf(LocalDate.ofEpochDay(endLocalDate.toEpochDay() + 1));
+
+                        for (Activity activity : activities) {
+                            if (activity.getDate().before(endDate)) {
+                                series1.getData().add(new XYChart.Data<>(activity.getDate().toString(), ActivityHelper.convertTimeStringToDouble(activity.getTime())));
+                            }
+                        }
+                    }
+
+                    dataChart.getData().addAll(series1);
+                });
+                break;
+        }
     }
 
     public void setActivities(ObservableList<Activity> activities) {
